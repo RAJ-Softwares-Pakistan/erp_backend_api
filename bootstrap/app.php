ed2d256ab42438db62fe58b4a1d889701468f96d
Taha Shakir <tahashakir40@gmail.com>
2025-05-04 23:35:34 +0500

    Added fix for 403

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: '*');
+        $middleware->trustHosts(at: [parse_url(env('APP_URL'), PHP_URL_HOST)]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
